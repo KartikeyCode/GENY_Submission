39fbf3c1d23066b4b137faa45a70d671af29f3f4
Kartikey <codekartikey@gmail.com>
2023-03-19 00:01:43 +0530

    add sidemenu items

--- geny/pages/sidebar.js
+++ geny/pages/sidebar.js
@@ -5,7 +5,16 @@ function Sidebar(){
         <div className="z-0 top-0 w-[12.5rem] slide fixed bg-neutral-800 h-screen">
         <div className="p-6 pt-20">
             <h1 className="text-xl text-slate-50 cursor-pointer mb-8">🏠 Home</h1>
-            <h1 className="text-xl text-slate-50 cursor-pointer">Subscriptions</h1>
+            <h1 className="text-xl text-slate-50 cursor-pointer mb-8">🍆 Subscriptions</h1>
+            <h1 className="text-xl text-slate-50 cursor-pointer mb-8">🔥 Trending</h1>
+            <h1 className="text-xl text-slate-50 cursor-pointer mb-8">👍🏿 Liked Videos</h1>
+            <h1 className="text-xl text-slate-50 cursor-pointer mb-8">🕒 Watch Later</h1>
+            <h1 className="text-xl text-slate-50 cursor-pointer mb-8">🎵 Music</h1>
+            <h1 className="text-xl text-slate-50 cursor-pointer mb-8">📚 Learning</h1>
+            <h1 className="text-xl text-slate-50 cursor-pointer mb-8">🎬 Movies</h1>
+            <h1 className="text-xl text-slate-50 cursor-pointer mb-8">🎮 Gaming</h1>
+            <h1 className="text-xl text-slate-50 cursor-pointer mb-8">📰 News</h1>
+            <h1 className="text-xl text-slate-50 cursor-pointer mb-8">🏆 Sports</h1>
         </div>
         </div>
         </>
